d7b690bfd078a8b97d5a9d6e27a856c6e2043716
Angel Reyna <rugile@protonmail.com>
2023-01-11 18:52:06 +0200

    commit

--- oop :practise/script.js
+++ oop :practise/script.js
@@ -11,27 +11,47 @@ class User {
         this.frequency = frequency;
         this.blessings = [];
     }
-
+    
+    info(){ 
+        return this.name + this.surname + this.chipnumber + this.addy + this.frequency
+    }
     //add blessings to the block that are claimed
     blessingsClaimed(blessing) {
-        let target = document.getElementById(this.name);
-        let element = document.createElement("p");
-        element.innerText = blessing;
-        target.appendChild(element).className = "bordered listitem";
+        // let target = document.getElementById(this.name);
+        // let element = document.createElement("form");
+        // element.innerText = blessing;
+        // target.appendChild(element).className = "bordered listitem";
+
         this.blessings.push(blessing);
     }
     check() {
         console.log(this.name + " claimed these blessings: ");
         for (let i = 0; i < this.blessings.length; i++) {
             console.log(this.blessings[i]);
+            console.log(this.blessings);
+            return this.name + this.blessings
         }
     }
 }
 
 
+const PinkDoll = new User (" PinkDoll " , " Misty ", " 1000101 ", " Pink Oak 777, Magic Forest ", " 440Hz ", " Abundance, Love, Shine, Joy ");
+const Butterfly = new User (" Butterfly ", " Winggs ", " 1110101 ", " Yellow Rose 11, Rainbow Field ", " 440Hz ", " Love, Passion, Taste ");
 
-const PinkDoll = new User ("PinkDoll", "Misty", "1000101", "Pink Oak 777, Magic Forest", "440Hz", "Abundance, Love, Shine, Joy");
-const Butterfly = new User ("Butterfly", "Winggs", "1110101", "Yellow Rose 11, Rainbow Field", "440Hz", "Love, Passion, Taste");
+console.log(PinkDoll.info());
+console.log(Butterfly.info());
+
+PinkDoll.blessingsClaimed("Abundance");
+PinkDoll.blessingsClaimed("Love");
+PinkDoll.blessingsClaimed("Shine");
+PinkDoll.blessingsClaimed("Joy");
+
+Butterfly.blessingsClaimed("Love");
+Butterfly.blessingsClaimed("Passion");
+Butterfly.blessingsClaimed("Taste");
+
+console.log(PinkDoll.check())
+console.log(Butterfly.check())
 
 
 
@@ -41,5 +61,9 @@ const Butterfly = new User ("Butterfly", "Winggs", "1110101", "Yellow Rose 11, R
 
 
 
-    //     console.log(this.name, this.surname, this.chipnumber, this.addy, this.frequency)
-    // 
+
+
+
+
+
+
