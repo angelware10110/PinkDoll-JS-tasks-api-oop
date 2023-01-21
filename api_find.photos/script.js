50f8628c482f27841409478456599af7cefb25b2
Angel Reyna <rugile@protonmail.com>
2023-01-21 15:43:54 +0200

    commit

--- api_find.photos/script.js
+++ api_find.photos/script.js
@@ -2,61 +2,50 @@ const main_input = document.querySelector('input');
 const main_btn = document.querySelector('button');
 
 const imgContainer = document.querySelector('.img-container');
-const apiKey = `sbcYgo2GTlRHz9etpt9i3BgvVAeE61U6XrK8xnuiqwg`
+const apiKey = `HLt57IqMaOHGScijvaMABIMdd8ZHgevDv9BhO7ZZQ9A`
 
 
-async function loadingPhotos() {
+async function loadPhotos() { //loadphotos
     
     const res = await fetch(`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=24`);
     const data = await res.json();
-
-
-    const newDiv = document.createElement('div');
+    const myDiv = document.createElement('div');
    
     data.map(item => {
 
-        newDiv.classList.add('deletible');
-        imgContainer.appendChild(newDiv)
-
+        myDiv.classList.add('photos');
+        imgContainer.appendChild(myDiv)
         const img = `<img src='${item.urls.small}'></img>`
-        
-        newDiv.innerHTML += img;
+        myDiv.innerHTML += img;
     });
 };
 
-
-
-
-
-async function getData(e) {
-    e.preventDefault();
+async function getData(event) { //choose photo
+    event.preventDefault();
     console.log(main_input.value)
     const word = main_input.value;
     const res = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${word}&client_id=${apiKey}&per_page=24`);
     const data = await res.json();
 
 
-    const newDiv = document.createElement('div');
+    const myDiv = document.createElement('div');
 
-    if (document.querySelector('.deletible') !== undefined) {
-      
-        document.querySelectorAll('.deletible').forEach(item => item.remove());
+    if (document.querySelector('.photos') !== undefined) {
+        document.querySelectorAll('.photos').forEach(item => item.remove());
 
     }
-
 
     console.log(data)
    if(data){data.results.map(item => {
 
-        newDiv.classList.add('deletible');
-        imgContainer.appendChild(newDiv)
-
+        myDiv.classList.add('photos');
+        imgContainer.appendChild(myDiv)
         const img = `<img src='${item.urls.small}'></img>`
-        
-        newDiv.innerHTML += img;
+
+        myDiv.innerHTML += img;
     })}
 };
 
-loadingPhotos();
+loadPhotos();
 
 main_btn.addEventListener('click', getData);
